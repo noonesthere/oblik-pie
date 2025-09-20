dc2b3e78b384b9f73e811acc956b9c51449c1559
NoOnesThere <d.zlobenets@gmail.com>
2025-09-20 16:10:25 +0300

    remove trash

--- oblik-ui/app/config/page.tsx
+++
@@ -1,8 +0,0 @@
-//  config
-// fileName.tsx as is
-//component from Uppercase and snake case
-export default function ConfigList() {
-  return <div>
-    <h3> Config </h3>
-  </div>
-}
--- oblik-ui/app/protocols/page.tsx
+++
@@ -1,8 +0,0 @@
-//  protocols
-// fileName.tsx as is
-//component from Uppercase and snake case
-export default function ProtocolsList() {
-return <div>
-    <h3> Protocols </h3>
-</div>
-}
